5ccbaedbf9574b787356e0af75014f288e98b983
Sirkkeli <siiri.kalliomaki@helsinki.fi>
2024-11-10 15:27:00 +0200

    Tehtävä 2 tehty

--- viikko2/project-reader/src/project.py
+++ viikko2/project-reader/src/project.py
@@ -1,17 +1,20 @@
+import tomllib
 class Project:
-    def __init__(self, name, description, dependencies, dev_dependencies):
-        self.name = name
-        self.description = description
-        self.dependencies = dependencies
-        self.dev_dependencies = dev_dependencies
-
+    def __init__(self, content):
+        toml_str  = tomllib.loads(content)
+        self.name = toml_str["tool"]["poetry"]["name"]
+        self.description = toml_str["tool"]["poetry"]["description"]
+        self.dependencies = toml_str["tool"]["poetry"]["dependencies"]
+        self.dev_dependencies = toml_str["tool"]["poetry"]["group"]["dev"]["dependencies"]
+        self.authors = toml_str["tool"]["poetry"]["authors"]
     def _stringify_dependencies(self, dependencies):
-        return ", ".join(dependencies) if len(dependencies) > 0 else "-"
+        return "\n - ".join(dependencies) if len(dependencies) > 0 else "-"
 
     def __str__(self):
         return (
             f"Name: {self.name}"
             f"\nDescription: {self.description or '-'}"
-            f"\nDependencies: {self._stringify_dependencies(self.dependencies)}"
-            f"\nDevelopment dependencies: {self._stringify_dependencies(self.dev_dependencies)}"
+            f"\n\n Authors: \n - {self._stringify_dependencies(self.authors)}"
+            f"\n\nDependencies: \n - {self._stringify_dependencies(self.dependencies)}"
+            f"\n\nDevelopment dependencies: \n - {self._stringify_dependencies(self.dev_dependencies)}"
         )
--- viikko2/project-reader/src/project_reader.py
+++ viikko2/project-reader/src/project_reader.py
@@ -1,6 +1,6 @@
 from urllib import request
 from project import Project
-
+import tomllib
 
 class ProjectReader:
     def __init__(self, url):
@@ -8,8 +8,14 @@ class ProjectReader:
 
     def get_project(self):
         # tiedoston merkkijonomuotoinen sisältö
-        content = request.urlopen(self._url).read().decode("utf-8")
-        print(content)
+        content = str(request.urlopen(self._url).read().decode("utf-8"))
+        # print("content:", content)
+        # toml_str  = tomllib.loads(content)
 
+        # print("toml_str: ",toml_str)
+
+        # print("a")
+        # print(toml_str["tool"]["poetry"]["description"])
         # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
-        return Project("Test name", "Test description", [], [])
+        return Project(content)
+
