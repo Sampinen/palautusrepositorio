02d223d2e1289968383c2e9acdf7011d205037fa
Sirkkeli <siiri.kalliomaki@helsinki.fi>
2024-11-11 22:16:06 +0200

    viikko 2, tehtävä 12

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,25 +1,13 @@
 import requests
-from player import Player
+from player import Player, PlayerReader, PlayerStats
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players"
-    response = requests.get(url).json()
-
-    # print("JSON-muotoinen vastaus:")
-    # print(response)
-
-    # players = []
-
-    # for player_dict in response:
-    #     player = Player(player_dict)
-    #     players.append(player)
-
-    # print("Oliot:")
-
-    # for player in players:
-    #     print(player)
-    return print(Player(response, "FIN"))
-
+    reader = PlayerReader(url).return_list()
+    stats = PlayerStats(reader)
+    players = stats.top_scorers_by_nationality("FIN")
+    for player in players:
+        print(player)
 
 if __name__ == "__main__":
     main()
--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -1,13 +1,27 @@
+import requests
+
 class Player:
     def __init__(self, dict1,nationality):
-        self.players = ""
+        self.players = []
         for player in dict1:
             if player["nationality"] == nationality:
                 sum1 = str(player["goals"] + player["assists"])
-                self.players += f"\n{player["name"]}    {player["team"]}  {player["goals"]} + {player["assists"]} = {sum1}"
+                self.players.append(f"{player["name"]}    {player["team"]}  {player["goals"]} + {player["assists"]} = {sum1}") 
         self.nationality = nationality
 
-    def __str__(self):
-        print(f"Players from {self.nationality}\n\n")
+    def playerlist(self):
 
-        return self.players
+        return [f"Players from {self.nationality}\n\n"]+ self.players
+
+class PlayerReader:
+    def __init__(self,url):
+        self.url = url
+    def return_list(self):
+        return requests.get(self.url).json()
+
+class PlayerStats:
+    def __init__(self,reader):
+        self.players = reader
+    def top_scorers_by_nationality(self,nationality):
+        players = Player(self.players,nationality)
+        return players.playerlist()
