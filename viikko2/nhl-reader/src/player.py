2f768bad5abe994afd93cfde74c84a6416d7ceff
Sirkkeli <siiri.kalliomaki@helsinki.fi>
2024-11-11 21:13:04 +0200

    viikko 2, tehtävä 10

--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -1,6 +1,12 @@
 class Player:
-    def __init__(self, dict):
-        self.name = dict['name']
-    
+    def __init__(self, dict1,nationality):
+        self.players = ""
+        for player in dict1:
+            if player["nationality"] == nationality:
+                 self.players += f"\n{player["name"]} team {player["team"]}  goals {player["goals"]}  assists {player["assists"]}"
+        self.nationality = nationality
+
     def __str__(self):
-        return self.name
+        print(f"Players from {self.nationality}\n\n")
+
+        return self.players
