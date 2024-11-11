7a5d39e793f4743d160e968c2a540694f9c4ff9e
Sirkkeli <siiri.kalliomaki@helsinki.fi>
2024-11-11 21:18:31 +0200

    viikko 2, tehtävä 11

--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -3,7 +3,8 @@ class Player:
         self.players = ""
         for player in dict1:
             if player["nationality"] == nationality:
-                 self.players += f"\n{player["name"]} team {player["team"]}  goals {player["goals"]}  assists {player["assists"]}"
+                sum1 = str(player["goals"] + player["assists"])
+                self.players += f"\n{player["name"]}    {player["team"]}  {player["goals"]} + {player["assists"]} = {sum1}"
         self.nationality = nationality
 
     def __str__(self):
